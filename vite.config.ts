e11d789ce97fac0ad90509f8c87b1cc12a2283b9
xHexed <58384565+xHexed@users.noreply.github.com>
2026-02-08 19:57:59 +0700

    feat: make dev env able to use ts paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react(), basicSsl(), tsconfigPaths()],
   server: {
-    https: true,
+    //https: true,
     port: 5173,
     host: true, // Allow external access
   },
