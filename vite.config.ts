309d719473467792a234a70d716f8237a3d6131b
himkhoa <nguyendangkhoaa.1922@gmail.com>
2026-02-23 16:25:29 +0700

    refactor: update Vite configuration to support environment variables and conditional HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), basicSsl(), tsconfigPaths()],
-  server: {
-    //https: true,
-    port: 5173,
-    host: true, // Allow external access
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const useHttps = env.VITE_USE_HTTPS === "true";
+
+  return {
+    plugins: [react(), ...(useHttps ? [basicSsl()] : []), tsconfigPaths()],
+    server: {
+      port: 5173,
+      host: true, // Allow external access
+    },
+  };
 });
